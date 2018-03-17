2fdce2278b2af5195223b1b47caa44c770fa441f
pcoded <peter88tom@gmail.com>
2018-03-17 16:33:04 +0200

    collect static files with django

--- admin/settings.py
+++ admin/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'http://localhost/Bug-tracker/static/'
+#STATIC_ROOT  = os.path.join(BASE_DIR, "static")
+
+MEDIA_ROOT = '/var/www/html/Bug-tracker/static/img/'
+MEDIA_URL = '/img/'
+
