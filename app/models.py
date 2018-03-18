4555255763f1aaacc4ecd16003c2db4c85dafa4a
pcoded <peter88tom@gmail.com>
2018-03-18 04:14:16 +0200

    Add app models to admin site

--- app/models.py
+++ app/models.py
@@ -4,12 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Project model.
-class Projects(models.Model):
+class Project(models.Model):
 	project_name = models.CharField(max_length=255, null=False, blank=False)
 
 
+
 # Bug model
-class Bugs(models.Model):
+class Bug(models.Model):
 	description = models.TextField()
-	project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+	project = models.ForeignKey(Project, on_delete=models.CASCADE)
 	date_added = models.DateField(null=False, blank=False)
