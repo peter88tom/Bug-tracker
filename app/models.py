76094b01bc2a43290f0110a11e60e294d6d2cba2
pcoded <peter88tom@gmail.com>
2018-03-18 03:43:32 +0200

    Add Bugs and Projects models

--- app/models.py
+++ app/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Project model.
+class Projects(models.Model):
+	project_name = models.CharField(max_length=255, null=False, blank=False)
+
+
+# Bug model
+class Bugs(models.Model):
+	description = models.TextField()
+	project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+	date_added = models.DateField(null=False, blank=False)
