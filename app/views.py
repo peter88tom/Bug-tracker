b836841bb31429b1b2d549625c7d9abc23d19e36
pcoded <peter88tom@gmail.com>
2018-03-18 05:44:31 +0200

    Add list of projects to dropdown

--- app/views.py
+++ app/views.py
@@ -3,10 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Bug
 
 # Create your views here.
 def homepage(request):
 	return render(request, 'index.html')
 
 def new_bug(request):
-	return render(request, 'new-bug.html')
+	# Get a list of projects
+	list_of_projects = Project.objects.all()
+
+	return render(request, 'new-bug.html', {"projects":list_of_projects})
