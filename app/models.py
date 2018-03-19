240cb1f9a2a12bd3219693956f17fbb4f8bef37a
pcoded <peter88tom@gmail.com>
2018-03-19 14:20:54 +0200

    Fix date added in Bug model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Project model.
 class Project(models.Model):
 	project_name = models.CharField(max_length=255, null=False, blank=False)
@@ -17,7 +18,7 @@ class Project(models.Model):
 class Bug(models.Model):
 	description = models.TextField()
 	project = models.ForeignKey(Project, on_delete=models.CASCADE)
-	date_added = models.DateField(null=False, blank=False)
+	date_added = models.DateTimeField(auto_now=True)
 
 
 	# Model return name of project
